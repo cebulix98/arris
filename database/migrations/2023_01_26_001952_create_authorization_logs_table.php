4d4cd8ef90780afc168cc8cc85fd81e6bde1c1a6
DESKTOP-72UBU1M\sszym <sszymanski098@gmail.com>
2023-01-26 01:25:46 +0100

    add authorization log model

---
+++ database/migrations/2023_01_26_001952_create_authorization_logs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authorization_logs', function (Blueprint $table) {
+            $table->string('action');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authorization_logs');
+    }
+};
